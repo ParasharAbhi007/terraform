0f482d92a667538ee609c1e658d5debea301370a
Abhinandan Parashar <43598084+ParasharAbhi007@users.noreply.github.com>
2025-08-26 16:16:08 +0530

    Update main.tf

--- exercise_3/main.tf
+++ exercise_3/main.tf
@@ -1,14 +1,14 @@
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
-  region     = "us-east-1"
+  region     = "us-east-2"
 }
 
 terraform {
   backend "s3" {
     bucket = "lijo-lab"        # bucket for remote state
     key    = "exercise_3/terraform.tfstate"  # required path inside the bucket
-    region = "us-east-1"
+    region = "us-east-2"
   }
 }
 
