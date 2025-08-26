1e2f87dd7489035b8b5021d4635ed1e2fdf583eb
Abhinandan Parashar <43598084+ParasharAbhi007@users.noreply.github.com>
2025-08-26 15:54:39 +0530

    Update main.tf

--- exercise_3/main.tf
+++ exercise_3/main.tf
@@ -1,27 +1,27 @@
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region = "${var.region}"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
 }
 
 terraform {
-   backend "s3" {
-   bucket = "rc-tf-bucket-training-exercise-11"
-   region = "us-east-1"
+  backend "s3" {
+    bucket = var.s3_bucket_name        # bucket for remote state
+    key    = "exercise_3/terraform.tfstate"  # required path inside the bucket
+    region = var.s3_bucket_region
   }
 }
 
-resource "aws_s3_bucket" "tf-root-module-bucket" {
-  bucket = "${var.s3_bucket_name}"
-  acl = "private"
-  
-  
+resource "aws_s3_bucket" "tf_root_module_bucket" {
+  bucket = var.s3_bucket_name
+  acl    = "private"
+
   versioning {
     enabled = true
   }
-  
+
   tags = {
-    Name        = "${var.s3_bucket_name}"
-    Environment = "${var.tag_env}"
+    Name        = var.s3_bucket_name
+    Environment = var.tag_env
   }
 }
